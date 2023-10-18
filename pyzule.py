a3875fd03e2d892f4532ac292df68cf606ce8d08
Lê Tí <106995604+arm64x@users.noreply.github.com>
2023-10-18 17:27:09 +0700

    Fix alias dylib

--- pyzule.py
+++ pyzule.py
@@ -286,7 +286,8 @@ if args.f:
         run(["tar", "-xf", data_tar, "-C", os.path.join(output, "e")], check=True)
         for dirpath, dirnames, filenames in os.walk(os.path.join(output, "e")):
             for filename in filenames:
-                if filename.endswith(".dylib") and not any(com in filename.lower() for com in common):
+                filepath = os.path.join(dirpath, filename)
+                if filename.endswith(".dylib") and not any(com in filename.lower() for com in common) and not os.path.islink(filepath):
                     src_path = os.path.join(dirpath, filename)
                     dest_path = os.path.join(DYLIBS_PATH, filename)
                     if not os.path.exists(dest_path):
